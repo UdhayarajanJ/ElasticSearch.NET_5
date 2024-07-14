6b448466fc20b5eeb69517128633d4c9eef700b1
Udhayarajan J <udhayarajanj@outlook.com>
2024-07-14 18:05:00 +0530

    [ add ] : required setup start implementations

--- ElasticSearch.Experiments/Program.cs
+++ ElasticSearch.Experiments/Program.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿// <copyright file="Program.cs" company="Anonymous Corp.">
+// Copyright (c) Anonymous Corp.. All rights reserved.
+// </copyright>
 
 namespace ElasticSearch.Experiments
 {
-    class Program
+    using System;
+
+    /// <summary>
+    /// Entry point of the files.
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Program"/> class.
+        /// </summary>
+        protected Program()
+        {
+        }
+
+        /// <summary>
+        /// Entry point of application main methods.
+        /// </summary>
+        /// <param name="args">Holds the pre requsit Arguments.</param>
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
