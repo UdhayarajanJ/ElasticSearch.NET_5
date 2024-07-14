aca428253d329ec696e4aa0df25abc2493444e0d
Udhayarajan J <udhayarajanj@outlook.com>
2024-07-14 08:20:02 +0530

    Add project files.

---
+++ ElasticSearch.Experiments/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElasticSearch.Experiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
